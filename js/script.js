0e04fed017b3e64c1cc4bf3e982b394ea5ba0606
Sasha <sasha.zhulega.02@mail.ru>
2023-10-06 20:43:42 +0300

    improve modal function

--- js/script.js
+++ js/script.js
@@ -106,10 +106,26 @@ window.addEventListener('DOMContentLoaded', () => {
     btnModal.forEach(item => {
         item.addEventListener('click', () => {
             modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';
         })
     })
 
-    btnClose.addEventListener('click', () => {
+    function closeModal () {
         modal.style.display = 'none';
-    });
+        document.body.style.overflow = '';
+    }
+
+    btnClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal) {
+            closeModal();    
+        }
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape') {
+            closeModal();
+        }
+    })
 });
